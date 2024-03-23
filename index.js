23acd3766112a0f0c47165aacc970a5e074d8872
belrado <u5ink@naver.com>
2024-03-24 01:45:23 +0900

    test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
-//import { name as appName } from './app.json';
+import { name as appName } from './app.json';
 import { Provider } from 'react-redux';
 import store from '@/features/store';
 import messaging from '@react-native-firebase/messaging';
